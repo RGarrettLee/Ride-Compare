6f668262e35587bef1be0a4df92752fadc06e9d0
liangying <wingkyleung1208@hotmail.com>
2022-06-01 11:46:26 -0400

    modal

---
+++ assets/script.js
@@ -0,0 +1,17 @@
+// modal showup 
+const rideModal = document.getElementById('rideModal')
+rideModal.addEventListener('show.bs.modal', event => {
+    // Button that triggered the modal
+    const button = event.relatedTarget
+
+    // Update the modal's destination
+
+    const modalBodyInput1 = exampleModal.querySelector('#destination')
+    modalBodyInput1.value = destination;
+
+    // Update the modal's start-point
+
+    const modalBodyInput2 = exampleModal.querySelector('#start-point')
+    modalBodyInput2.value = start - Point;
+})
+
